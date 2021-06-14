1f2c7a70490620528a55a40cc2970d114a5abe5b
Raul Menjivar <raul.menjivar4@gmail.com>
2021-06-14 13:04:32 -0500

    Added Bootstrap Partials and CSS

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -5,6 +7,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 
@@ -12,6 +15,14 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
+app.get("/store", function(req, res) {
+  res.render("store");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
 app.listen(3000, function(req, res){
   console.log("Now listening on Port 3000");
-})
+});
